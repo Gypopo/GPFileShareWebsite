eee6c36573626c2cbe15c57f53d27f39b002030c
BuildTools <52633285+Gypopo@users.noreply.github.com>
2023-03-03 19:49:48 +0100

    Some progress

--- style.js
+++ style.js
@@ -1,10 +1,28 @@
+var id = 0;
 var cardList = document.getElementById('cards');
 
 function appendCard(card) {
     cardList.appendChild(card);
 }
 
-function createCard() {
+function removeCard(card) {
+    cardList.removeChild(card);
+}
+
+function createCard(id) {
+    var card = document.createElement('div');
+    document.onclick = function(){displayCard}
+    var id = 1;
+    card.className = 'card';
+    
+    return card;
+}
+
+function displayCard() {
+    cardList.removeChild(card);
+}
+
+function addCard(author, versions, description, files) {
     var card = document.createElement('div');
     card.className = 'card';
     
