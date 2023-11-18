2d4deec3d7d9c09493fa64eb1e28820da3873f18
BuildTools <ferreschillemans@gmail.com>
2023-11-19 00:08:18 +0100

    Cleanup

--- tags.js
+++
@@ -1,3 +0,0 @@
-function loadTags(text) {
-    if (text.includes('#'));
-}
--- test.js
+++
@@ -1,6 +0,0 @@
-import {Card} from './objects/Card.js';
-createCard();
-
-function createCard() {
-    var cards = new Card(1, 'Gypopo', "Cool looking gui layout's", '4am', '#op, #skyblock #survival #basic #cheap #modern #oldSchool french, everyItem', '5.2.4', '1.19, 1.18');
-}
